31987aed204015d51a4922fa42ec050d97838d37
Jonathan  Foster <jfoster91@192.168.1.108>
2018-07-23 16:46:55 +0100

    Add basic sign up

--- app/src/main/java/com/example/jfoster91/kindleshare/MainActivity.kt
+++ app/src/main/java/com/example/jfoster91/kindleshare/MainActivity.kt
@@ -2,11 +2,55 @@ package com.example.jfoster91.kindleshare
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.AuthResult
+import com.google.android.gms.tasks.Task
+import android.support.annotation.NonNull
+import com.google.android.gms.tasks.OnCompleteListener
+import android.R.attr.password
+import android.support.v4.app.FragmentActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    val mAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentUser = mAuth.currentUser
+
+        if(currentUser != null){
+            login()
+        }
+    }
+
+
+    fun clickedLoginOrRegister(view: View){
+
+        mAuth.createUserWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.i("Jonnybag", "Success")
+                        login()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+    }
+
+    fun login(){
+        Log.i("Login", "Success")
     }
 }
+
+
