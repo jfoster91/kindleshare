30826a4b2824cb106896990905365ee5d459f293
Jonathan  Foster <jfoster91@192.168.1.108>
2018-07-24 10:15:25 +0100

    Add Basic DB

--- app/src/main/java/com/example/jfoster91/kindleshare/MainActivity.kt
+++ app/src/main/java/com/example/jfoster91/kindleshare/MainActivity.kt
@@ -16,11 +16,18 @@ import android.support.v4.app.FragmentActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import android.R.attr.password
 import android.content.Intent
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.firebase.firestore.DocumentReference
+import com.google.android.gms.tasks.OnSuccessListener
+
 
 
 class MainActivity : AppCompatActivity() {
 
     val mAuth = FirebaseAuth.getInstance()
+    val db = FirebaseFirestore.getInstance()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,19 +38,25 @@ class MainActivity : AppCompatActivity() {
         if(currentUser != null){
             login()
         }
+
+
     }
 
 
     fun clickedLoginOrRegister(view: View){
 
         var tag = goBtn.getTag()
+        val userEmail = emailEditText.text.toString()
+        val userPassword = passwordEditText.text.toString()
 
         if (tag == "Register") {
 
-            mAuth.createUserWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString())
+            mAuth.createUserWithEmailAndPassword(userEmail, userPassword)
                     .addOnCompleteListener(this) { task ->
                         if (task.isSuccessful) {
+                            addToDB()
                             login()
+
                         } else {
                             // If sign in fails, display a message to the user.
                             Toast.makeText(this, "Authentication failed.",
@@ -52,7 +65,7 @@ class MainActivity : AppCompatActivity() {
                     }
 
         } else if (tag == "SignIn") {
-            mAuth.signInWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString())
+            mAuth.signInWithEmailAndPassword(userEmail, userPassword)
                     .addOnCompleteListener(this) { task ->
                         if (task.isSuccessful) {
                             login()
@@ -69,6 +82,21 @@ class MainActivity : AppCompatActivity() {
         Log.i("Login", "Success")
         val loginIntent = Intent(this, IndexActivity::class.java)
         startActivity(loginIntent)
+    }
+
+    fun addToDB(){
+        // Create a new user with a first and last name
+        val userEmail = emailEditText.text.toString()
+        val user = HashMap<String, Any>()
+        user.put("email", userEmail)
+        user.put("tokens", 4)
+
+
+// Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener { documentReference -> Log.i("Result", "DocumentSnapshot added with ID: " + documentReference.id) }
+                .addOnFailureListener { e -> Log.i("Result", "Error adding document", e) }
     }
 }
 
